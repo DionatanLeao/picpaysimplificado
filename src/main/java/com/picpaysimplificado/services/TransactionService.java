ff8b63c4d55d18783300d204fd06d467ee2c3f73
DionatanLeao <dionatan.ti@outlook.com>
2023-12-03 14:58:45 -0300

    Testing service success

--- src/main/java/com/picpaysimplificado/services/TransactionService.java
+++ src/main/java/com/picpaysimplificado/services/TransactionService.java
@@ -5,14 +5,9 @@ import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.dtos.TransactionDTO;
 import com.picpaysimplificado.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Map;
 
 @Service
 public class TransactionService {
@@ -26,7 +21,7 @@ public class TransactionService {
     private NotificationService notificationService;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private AuthorizationService authorizationService;
 
     public Transaction createTransaction(TransactionDTO transaction) throws Exception {
         User sender = userService.findUserById(transaction.senderId());
@@ -34,8 +29,7 @@ public class TransactionService {
 
         userService.validateTransaction(sender, transaction.value());
 
-        boolean isAuthorized = authorizeTransaction(sender, transaction.value());
-
+        boolean isAuthorized = authorizationService.authorizeTransaction(sender, transaction.value());
 
         if (!isAuthorized) {
             throw new Exception("Transação não autorizada");
@@ -58,14 +52,5 @@ public class TransactionService {
         notificationService.sendNotification(receiver, "Transação recebida com sucesso");
 
         return newTransaction;
-    }
-
-    public boolean authorizeTransaction(User sender, BigDecimal value) {
-        ResponseEntity<Map> authorizationResponse =
-                restTemplate.getForEntity("https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6", Map.class);
-        if (authorizationResponse.getStatusCode() == HttpStatus.OK) {
-            String message = (String) authorizationResponse.getBody().get("message");
-            return "Autorizado".equalsIgnoreCase(message);
-        } else return false;
     }
 }
